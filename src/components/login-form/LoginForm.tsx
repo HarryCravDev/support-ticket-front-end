45d3d6693560986dfdf9de3c27215de4ade2a327
Harry Craven <HarryCraven.dev@gmail.com>
2022-07-08 13:15:25 +0100

    feature/login-screen

---
+++ src/components/login-form/LoginForm.tsx
@@ -0,0 +1,90 @@
+import { Button, Checkbox, Form, Input, Alert } from "antd";
+import React, { useState } from "react";
+import { useAppDispatch, useAppSelector } from "../../app/hooks";
+import { userLoginAsync, selectUser } from "../../slices/userSlices";
+import { useNavigate } from "react-router-dom";
+
+const LoginForm: React.FC = () => {
+	const dispatch = useAppDispatch();
+	const { isLoggedIn } = useAppSelector(selectUser);
+	const navigate = useNavigate();
+	const [showAlert, setShowAlert] = useState(false);
+	const [alertMessage, setAlertMessage] = useState("");
+
+	const onFinish = async (values: { email: string; password: string }) => {
+		console.log("Success:", values);
+		const res = await dispatch(userLoginAsync(values));
+		console.log("Harry Crave: ", res);
+
+		if (res.payload.user) {
+			navigate("/");
+		} else {
+			setShowAlert(true);
+			setAlertMessage(res.payload.response.data.message);
+			const timer = setTimeout(() => {
+				setShowAlert(false);
+				clearTimeout(timer);
+			}, 3000);
+		}
+	};
+
+	const onFinishFailed = (errorInfo: any) => {
+		console.log("Failed:", errorInfo);
+	};
+
+	return (
+		<div>
+			{showAlert && (
+				<Alert
+					style={{ marginBottom: "1rem" }}
+					message={alertMessage}
+					type="error"
+					showIcon
+				/>
+			)}
+			<Form
+				name="basic"
+				// labelCol={{ span: 8 }}
+				// wrapperCol={{ span: 16 }}
+				initialValues={{ remember: true }}
+				onFinish={onFinish}
+				onFinishFailed={onFinishFailed}
+				autoComplete="off"
+				style={{ width: "500px" }}
+				layout="vertical"
+			>
+				<Form.Item
+					label="Email"
+					name="email"
+					rules={[{ required: true, message: "Please input your email!" }]}
+				>
+					<Input />
+				</Form.Item>
+
+				<Form.Item
+					label="Password"
+					name="password"
+					rules={[{ required: true, message: "Please input your password!" }]}
+				>
+					<Input.Password />
+				</Form.Item>
+
+				{/* <Form.Item
+				name="remember"
+				valuePropName="checked"
+				wrapperCol={{ offset: 8, span: 16 }}
+			>
+				<Checkbox>Remember me</Checkbox>
+			</Form.Item> */}
+
+				<Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+					<Button type="primary" htmlType="submit">
+						Submit
+					</Button>
+				</Form.Item>
+			</Form>
+		</div>
+	);
+};
+
+export default LoginForm;
---
+++ src/screens/login-screen/Login.tsx
@@ -0,0 +1,17 @@
+import { Typography } from "antd";
+import React from "react";
+import LoginForm from "../../components/login-form/LoginForm";
+import "./login.css";
+
+const Login = () => {
+	return (
+		<section className="login-container" style={{ height: "91vh" }}>
+			<Typography.Title level={1} style={{ marginBottom: "1.5rem" }}>
+				Login to your account
+			</Typography.Title>
+			<LoginForm />
+		</section>
+	);
+};
+
+export default Login;
