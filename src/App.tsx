4334fee43ae082fa9c4f8e10deb9fe5dad8ee242
Harry Craven <HarryCraven.dev@gmail.com>
2022-07-08 11:15:17 +0100

    feature/routes-added

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Header from "./components/header/Header";
+import Drawer from "./components/drawer/Drawer";
 
 function App() {
-	return <div className="App"></div>;
+	return (
+		<>
+			<Router>
+				<Header />
+				<Drawer />
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/login" element={<Login />} />
+					<Route path="/register" element={<Register />} />
+				</Routes>
+			</Router>
+		</>
+	);
 }
 
 export default App;
