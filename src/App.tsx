92ff07fbd4dfe75c93ec577ab2a1a063c40e4991
Harry Craven <HarryCraven.dev@gmail.com>
2022-07-13 10:13:08 +0100

    feature/adding-components-to-protected-route

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,9 @@ import Register from "./screens/register-screen/Register";
 import Header from "./components/header/Header";
 import Drawer from "./components/drawer/Drawer";
 import TicketMenu from "./screens/ticket-menu-screen/TicketMenu";
+import PrivateRoute from "./components/private-route/PrivateRoute";
+import NewTicket from "./screens/new-ticket/NewTicket";
+import DisplayTickets from "./screens/display-tickets-screen/DisplayTickets";
 
 function App() {
 	return (
@@ -17,6 +20,12 @@ function App() {
 					<Route path="/login" element={<Login />} />
 					<Route path="/register" element={<Register />} />
 					<Route path="/ticket-menu" element={<TicketMenu />} />
+					<Route path="/new-ticket" element={<PrivateRoute />}>
+						<Route path="/new-ticket" element={<NewTicket />} />
+					</Route>
+					<Route path="/tickets" element={<PrivateRoute />}>
+						<Route path="/tickets" element={<DisplayTickets />} />
+					</Route>
 				</Routes>
 			</Router>
 		</>
