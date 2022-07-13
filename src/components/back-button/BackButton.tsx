94f3a862fdb7cc6a1d4725646ab22fabcdf4950c
Harry Craven <HarryCraven.dev@gmail.com>
2022-07-13 10:07:46 +0100

    feature/back-button-component

--- src/components/back-button/BackButton.tsx
+++ src/components/back-button/BackButton.tsx
@@ -1,11 +1,22 @@
 import { RollbackOutlined } from "@ant-design/icons";
+import { Button } from "antd";
 import { Link } from "react-router-dom";
 
 const BackButton = ({ url }: { url: string }) => {
 	return (
-		<Link to={url}>
-			<RollbackOutlined /> Back
-		</Link>
+		<Button
+			style={{
+				position: "absolute",
+				top: 0,
+				left: 0,
+				marginTop: "0.5rem",
+				marginLeft: "0.5rem",
+			}}
+		>
+			<Link to={url}>
+				<RollbackOutlined /> Back
+			</Link>
+		</Button>
 	);
 };
 export default BackButton;
