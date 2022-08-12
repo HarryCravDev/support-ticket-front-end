5deb38aa5b7f03ef0f0f29d5b742dfd529673293
Harry Craven <HarryCraven.dev@gmail.com>
2022-08-12 10:01:42 +0100

    feature/ticket-table

--- src/components/ticket-table/TicketTable.tsx
+++ src/components/ticket-table/TicketTable.tsx
@@ -27,6 +27,22 @@ const TicketTable = () => {
 		return <Spin />;
 	}
 
+	const getProductFilters = () => {
+		const filters: any = tickets
+				.map(ticket => { return { text: ticket.product, value: ticket.product } })
+				.filter((value: any, index: number, self: any[]) => index === self.findIndex((t) => t.text === value.text));
+		return filters;
+	}
+
+	const getDateFilters = () => {
+		const filters: any = tickets.map(ticket => {
+
+			const date = new Date(ticket.createdAt);
+			return { text: date.toLocaleDateString(), value: date.toLocaleDateString()
+		}}).filter((value: any, index: number, self: any[]) => index === self.findIndex((t) => t.text === value.text));
+		return filters;
+	}
+
 	return (
 		<Table dataSource={tickets}>
 			<Column
@@ -34,13 +50,35 @@ const TicketTable = () => {
 				dataIndex="createdAt"
 				key="createdAt"
 				render={(date: string) => new Date(date).toLocaleString()}
+				filters={getDateFilters()}
+				filterSearch={true}
+				onFilter={(value: string | number | boolean, record: any) => {
+					const date = new Date(record.createdAt);
+					return date.toLocaleDateString().startsWith(value as string)
+				}}
+				responsive={['md']}
 			/>
-			<Column title="Product" dataIndex="product" key="product" />
+			<Column 
+				title="Product" 
+				dataIndex="product" 
+				key="product"
+				filters={getProductFilters()}
+				filterSearch={true}
+				onFilter={(value: string | number | boolean, record: any) => record.product.startsWith(value)} 
+				/>
 			<Column
 				title="Status"
 				dataIndex="status"
 				key="status"
 				className="status-column"
+				filters={
+					[
+						{ text: "open", value: "open" },
+						{ text: "in progress", value: "in progress" },
+						{ text: "closed", value: "closed" },
+					]
+				}
+				onFilter={(value: string | number | boolean, record: any) => record.status.indexOf(value) === 0}
 				render={(status: string) => (
 					<Tag
 						color={
@@ -64,6 +102,7 @@ const TicketTable = () => {
 					<Button
 						type="primary"
 						onClick={() => navigate(`/tickets/${ticket._id}`)}
+						key={ticket._id}
 					>
 						View
 					</Button>
